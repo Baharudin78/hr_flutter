94394487dc543cb4b2affb8a8ff8f3a41a979e4e
Baharudin78 <baharudinmaulana78@gmail.com>
2022-05-31 10:37:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/soldig/hr_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.soldig.hr_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:hr_flutter/view/splash/splashscreen.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.SPLASH;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashScreen(),
+    ),
+  ];
+}
